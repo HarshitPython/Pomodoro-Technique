3f1546438776b3dfd3c99376cc4c8c4fd9774580
Harshit Shrivastava <88823245+HarshitPython@users.noreply.github.com>
2021-09-29 13:12:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
-# GREEN = "#9bdeac"
-# YELLOW = "#f7f5dd"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
 # FONT_NAME = "Courier"
 # WORK_MIN = 25
 # SHORT_BREAK_MIN = 5
