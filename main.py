86c6e47ee75c3dca8a274d77533d251f9e2b4238
Harshit Shrivastava <88823245+HarshitPython@users.noreply.github.com>
2021-09-30 13:30:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ timer = None
 # # ---------------------------- TIMER RESET ------------------------------- # 
 
 def reset_timer():
-#     window.after_cancel(timer)
+    window.after_cancel(timer)
 #     title_label.config(text="Timer")
 #     canvas.itemconfig(timer_text,text="00:00")
 #     tick_label.config(text="")
