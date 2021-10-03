95a8d347d01df9c234f0de66ff9c7acf3084699d
Harshit Shrivastava <88823245+HarshitPython@users.noreply.github.com>
2021-10-03 23:22:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def count_down(count):
         start_timer()
         marks = ""
         work_sessions = math.floor(reps/2)
-#         for _ in range(work_sessions):
+        for _ in range(work_sessions):
 #             marks += "✔"
 #         tick_label.config(text=marks)
 # # ---------------------------- UI SETUP ------------------------------- #
