6ad7ea0fa1cdc736b0da2ba8ee717585c61f6115
Harshit Shrivastava <88823245+HarshitPython@users.noreply.github.com>
2021-10-05 13:43:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -76,9 +76,6 @@ window = Tk()
 window.title("Pomodoro")
 window.configure(padx=100, pady=50,bg=YELLOW)
 
-
-
-
 title_label = Label(text="Timer",fg=GREEN,bg=YELLOW,font=(FONT_NAME,50))
 title_label.grid(row=0,column=1)
 
@@ -93,7 +90,7 @@ canvas.grid(row=1,column=1)
 start_button = Button(text="START",highlightthickness=0,command = start_timer)
 start_button.grid(row=2,column=0)
 
-# tick_label = Label(fg=GREEN,bg=YELLOW)
+tick_label = Label(fg=GREEN,bg=YELLOW)
 # tick_label.grid(row=3,column=1)
 
 # reset_button = Button(text="RESET",highlightthickness=0,command = reset_timer)
